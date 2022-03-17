35e9ef88992f782fa1c5c2a7a7d23d1aa2fd6f7d
chandra15 <chandra.spdev15@gmail.com>
2022-03-17 17:04:16 +0530

    token

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 from github import Github
+import os
 
 # Creating this to mass update all of our repos settings.
 
@@ -8,21 +9,21 @@ from github import Github
 
 # Uses https://github.com/PyGithub/PyGithub
 
-g = Github("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") # Your personal token
+GH_TOKEN = os.environ['GH_TOKEN']
 
 def change_protected_branch_settings():
     # Loops through all the repos and changes the /settings/branch_protection_rules/
     # CAREFUL with this!
-    for repo in g.get_user().get_repos():
+    for repo in GH_TOKEN .get_user().get_repos():
         branch = repo.get_branch("main")
         branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
         print("Edited the branch protection rules for: " + repo.name)
 
 def change_protected_branch_settings_test():
-    repo = g.get_repo("Chandrasekhar674/test-ghpage")
-    branch = g.get_repo("Chandrasekhar674/test-ghpage").get_branch("main")
+    repo = GH_TOKEN .get_repo("Chandrasekhar674/test-ghpage")
+    branch = GH_TOKEN .get_repo("Chandrasekhar674/test-ghpage").get_branch("main")
     branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
 
 # Uncomment out one of these lines. Test to test a single repo/branch.
-#change_protected_branch_settings()
-#change_protected_branch_settings_test()
+change_protected_branch_settings()
+change_protected_branch_settings_test()
